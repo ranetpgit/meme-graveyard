8344059f5664137483da067be1430cda048fd135
sfhsdfasg <gertpaimla@gmail.com>
2018-06-30 15:37:16 +0300

    landing page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+    imports: [
+        SharedModule
+    ],
+    declarations: [],
+    providers: [
+
+    ]
+})
+export class CoreModule {
+
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        if (parentModule) {
+            throw new Error(
+                'CoreModule is already loaded. Import it in the AppModule only');
+        }
+    }
+}
